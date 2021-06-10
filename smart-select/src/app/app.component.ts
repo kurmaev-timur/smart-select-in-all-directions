60cdbbac69f1bdca0f9eb5d2ea0ed82292ae1a7a
Timur Kurmaev <timur95trigan@yandex.ru>
2021-06-10 23:12:30 +0300

    added basic logic

--- smart-select/src/app/app.component.ts
+++ smart-select/src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { MatOption } from '@angular/material/core/option';
+import { MatSelect, MatSelectChange } from '@angular/material/select';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +12,7 @@ import { FormControl } from '@angular/forms';
 export class AppComponent {
   
   //Creating source object with data about students
-  srcObject = [{
+  firstSrcObject: any[] = [{
     student: 'John',
     subject: 'Math',
     group : '1A',
@@ -34,22 +37,123 @@ export class AppComponent {
     subject: 'English',
     group : '1A',
     year: '1'
+  },{
+    student: 'Kate',
+    subject: 'Math',
+    group : '2A',
+    year: '2'
+  },{
+    student: 'Ivan',
+    subject: 'Math',
+    group : '2A',
+    year: '2'
   }]
 
+  srcObject: any[] = [];
+
   students = new FormControl();
-  studentsList: any[] = this.srcObject.map(s => s.student)
+  studentsList: any[] = [];
   
 
   subjects = new FormControl();
-  subjectsList: any[] = this.srcObject.map(s => s.subject)
-
+  subjectsList: any[] = [];
+ 
   groups = new FormControl();
-  groupsList: any[] = this.srcObject.map(s => s.group)
+  groupsList: any[] = [];
+
+
 
   ngOnInit(){
-    //Removing duplicate values in Lists
+
+    this.fillLists();
+    this.removeDuplicatesFromLists();
+  }
+
+  fillLists(){
+
+    this.srcObject = this.firstSrcObject;
+    this.studentsList = this.srcObject.map(s => s.student);
+    this.subjectsList = this.srcObject.map(s => s.subject)
+    this.groupsList = this.srcObject.map(s => s.group)
+  }
+  removeDuplicatesFromLists(){
     this.studentsList = [...new Set(this.studentsList)];
     this.subjectsList = [...new Set(this.subjectsList)];
     this.groupsList = [...new Set(this.groupsList)];
+  }
+
+  selectAllValues(){
+   
+  }
+
+  flexibleSelect(array :any[], item:string){
+    this.srcObject = [];
+    this.firstSrcObject.forEach(data => {
+      array.forEach(element => {
+        switch(item) { 
+          case "students": { 
+              if(data.student == element) {
+                console.log(data.item)
+                this.srcObject.push(data)
+              }
+             break; 
+          } 
+          case "subjects": { 
+            if(data.subject == element) {
+              console.log(data.item)
+              this.srcObject.push(data)
+            }
+             break; 
+          } 
+          case "groups": {
+            if(data.group == element) {
+              console.log(data.item)
+              this.srcObject.push(data)
+            }
+             break;    
+          } 
+       }
+      })
+    })
+  }
+
+  selectStudentItem(event : MatSelectChange){
+
+    if (event.value == "") {
+      this.fillLists();
+    }
+    else{
+      this.flexibleSelect(event.value , 'students');
+      console.log(this.srcObject)
+      this.subjectsList = this.srcObject.map(s => s.subject)
+      this.groupsList= this.srcObject.map(s => s.group)
+    }
+    this.removeDuplicatesFromLists();
+  }
+ 
+  selectSubjectItem(event : MatSelectChange){
+
+    if (event.value == "") {
+      this.fillLists();
+    }
+    else{
+      this.flexibleSelect(event.value,  'subjects');
+      this.studentsList = this.srcObject.map(s => s.student)
+      this.groupsList= this.srcObject.map(s => s.group)
+    }
+    this.removeDuplicatesFromLists();
+
+  }
+
+  selectGroupItem(event : MatSelectChange){
+    if (event.value == "") {
+      this.fillLists();
+    }
+    else{
+      this.flexibleSelect(event.value, 'groups');
+      this.subjectsList = this.srcObject.map(s => s.subject)
+      this.studentsList = this.srcObject.map(s => s.student)
+    }
+    this.removeDuplicatesFromLists();
   }
 }
