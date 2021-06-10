cd2c391cc9581b7b84ecc0ee7d4056b7d48bb982
Timur Kurmaev <timur95trigan@yandex.ru>
2021-06-10 22:01:37 +0300

    added 3 simple mat-select and source object

--- smart-select/src/app/app.component.ts
+++ smart-select/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'smart-select';
+  
+  //Creating source object with data about students
+  srcObject = [{
+    student: 'John',
+    subject: 'Math',
+    group : '1A',
+    year: '1'
+  },{
+    student: 'Mark',
+    subject: 'English',
+    group : '1A',
+    year: '1'
+  },{
+    student: 'Maria',
+    subject: 'English',
+    group : '1A',
+    year: '1'
+  },{
+    student: 'Andrew',
+    subject: 'English',
+    group : '1A',
+    year: '1'
+  },{
+    student: 'Alex',
+    subject: 'English',
+    group : '1A',
+    year: '1'
+  }]
+
+  students = new FormControl();
+  studentsList: any[] = this.srcObject.map(s => s.student)
+  
+
+  subjects = new FormControl();
+  subjectsList: any[] = this.srcObject.map(s => s.subject)
+
+  groups = new FormControl();
+  groupsList: any[] = this.srcObject.map(s => s.group)
+
+  ngOnInit(){
+    //Removing duplicate values in Lists
+    this.studentsList = [...new Set(this.studentsList)];
+    this.subjectsList = [...new Set(this.subjectsList)];
+    this.groupsList = [...new Set(this.groupsList)];
+  }
 }
